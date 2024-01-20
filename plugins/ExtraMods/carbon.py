2c68abc0a719694909010e94cf9a04b262f5d824
Youesky <135997849+youesky@users.noreply.github.com>
2024-01-20 19:29:06 +0530

    Update To V4.5.0

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -30,8 +30,8 @@ async def carbon_func(b, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**ᴍᴀᴅᴇ ʙʏ: @ebiza**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/ScenepackOfficial")]]),                   
     )
     await m.delete()
     carbon.close()

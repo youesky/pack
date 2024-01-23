e13fbb8a2e618c2df35a893e4cd93c652a28fbea
Youesky <135997849+youesky@users.noreply.github.com>
2024-01-23 17:23:08 +0530

    Update To V4.5.0

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -91,7 +91,7 @@ async def pm_spoll_tester(bot, query):
         k = (movie, files, offset, total_results)
         await pm_AutoFilter(bot, query, k)
     else:
-        k = await query.message.edit('Tʜɪs Mᴏᴠɪᴇ Nᴏᴛ Fᴏᴜɴᴅ Iɴ Dᴀᴛᴀʙᴀsᴇ')
+        k = await query.message.edit('Tʜɪs Sᴄᴇɴᴇᴘᴀᴄᴋ Nᴏᴛ Fᴏᴜɴᴅ Iɴ Dᴀᴛᴀʙᴀsᴇ')
         await asyncio.sleep(10)
         await k.delete()
 

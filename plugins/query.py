cac4db95bfbd365148b9d780006df2d6bc1b7b01
fousiyatc@gmail.com <fousiyatc@gmail.com>
2024-01-30 18:41:48 +0530

    Update To V4.5.0

--- plugins/query.py
+++ plugins/query.py
@@ -497,7 +497,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if query.from_user.id not in ADMINS:
             return await query.answer("Sᴏʀʀʏ Tʜɪs Mᴇɴᴜ Oɴʟʏ Fᴏʀ Mʏ Sᴄᴇɴᴇᴘᴀᴄᴋ Uᴘʟᴏᴀᴅᴇʀs 📤", show_alert=True)
         await query.message.edit("Pʀᴏᴄᴇꜱꜱɪɴɢ Wᴀɪᴛ Fᴏʀ 5 ꜱᴇᴄ...")
-        await query.edit_message_media(InputMediaPhoto(random.choice(PICS), script.SUDO_TXT, enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons)))
+        await query.edit_message_media(InputMediaPhoto(random.choice(PICS), script.SUDO_TXT, enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
         
     elif query.data == "openfilter":
         buttons = [[
